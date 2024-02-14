fe48c71a3f41d899413ce851766284f69cb86cec
sunhye <tjsgp43@gmail.com>
2024-02-14 15:32:18 +0900

    회원가입 연락처 패턴 수정

--- app/(main)/(sub)/contact/contactClient.tsx
+++ app/(main)/(sub)/contact/contactClient.tsx
@@ -3,6 +3,7 @@
 import { Btn } from '@/component/common/Btn/Btn';
 import style from './contactClient.module.scss';
 import SectionCommonLayout from '@/component/common/SectionCommonLayout/SectionCommonLayout';
+import Input from '@/component/common/Input/Input';
 
 export default function ContactClient() {
   return (
@@ -12,22 +13,35 @@ export default function ContactClient() {
       desc={'문의사항을 남겨주시면 <br />확인 후 연락드리겠습니다.'}
       btnYn={false}
     >
-      <div className={''}>
+      <div className={style.contact_box}>
         <div className={style.type}>
           <p className={style.title}>문의구분</p>
+          <div className={style.inp_box}>
+            <Input id={''} labelNm={''} type={''} value={''} ref={null} />
+          </div>
         </div>
 
         <div className={style.comp}>
           <p className={style.title}>회사명</p>
+          <div className={style.inp_box}>
+            <Input id={''} labelNm={''} type={''} value={''} ref={null} />
+          </div>
           <p className={style.title}>담당자명</p>
+          <div className={style.inp_box}>
+            <Input id={''} labelNm={''} type={''} value={''} ref={null} />
+          </div>
         </div>
 
         <div className={style.email}>
           <p className={style.title}>이메일</p>
+          <div className={style.inp_box}>
+            <Input id={''} labelNm={''} type={''} value={''} ref={null} />
+          </div>
         </div>
 
         <div className={style.content}>
           <p className={style.title}>문의내용</p>
+          <div className={style.inp_box}>{/* editor */}</div>
         </div>
 
         <div className={style.agree}></div>
