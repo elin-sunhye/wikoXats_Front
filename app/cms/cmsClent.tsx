68098db75d24b9c8e94d07dec5183db44b90da80
sunhye <tjsgp43@gmail.com>
2024-05-16 14:57:35 +0900

    파트너스 추가 및 메인비주얼 슬라이드 추가

--- app/cms/cmsClent.tsx
+++ app/cms/cmsClent.tsx
@@ -1,8 +1,4 @@
-import MainVisual from '@/component/main/MainVisual';
-import SectionCount from '@/component/main/SectionCount/SectionCount';
-import SectionProducts from '@/component/main/SectionService';
-import SectionService from '@/component/main/SectionService/SectionService';
-import SectionStrength from '@/component/main/SectionStrength';
+'use client';
 
 export default function CmsClent() {
   return <>대시보드</>;
