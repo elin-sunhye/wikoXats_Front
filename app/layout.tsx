1b23f347b60965f06c2f6333f5266cdc9efb54d3
sunhye <tjsgp43@gmail.com>
2024-05-17 11:13:48 +0900

    story 지도 작업

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,76 +1,76 @@
-import AutoAlert from "@/component/common/AutoAlert/AutoAlert";
-import { Footer } from "@/component/common/Footer/Footer";
-import { Header } from "@/component/common/Header/Header";
-import ReactQueryProvider from "@/component/common/ReactQueryProvider";
-import RecoilRootProvider from "@/component/common/RecoilRootProvider";
-import "@/style/globals.scss";
-import localFont from "next/font/local";
+import AutoAlert from '@/component/common/AutoAlert/AutoAlert';
+import { Footer } from '@/component/common/Footer/Footer';
+import { Header } from '@/component/common/Header/Header';
+import ReactQueryProvider from '@/component/common/ReactQueryProvider';
+import RecoilRootProvider from '@/component/common/RecoilRootProvider';
+import '@/style/globals.scss';
+import localFont from 'next/font/local';
 
 // font
 const pretendard = localFont({
   src: [
     {
-      path: "../fonts/Pretendard/Pretendard-Light.woff2",
-      weight: "300",
-      style: "normal",
+      path: '../fonts/Pretendard/Pretendard-Light.woff2',
+      weight: '300',
+      style: 'normal',
     },
     {
-      path: "../fonts/Pretendard/Pretendard-Regular.woff2",
-      weight: "400",
-      style: "normal",
+      path: '../fonts/Pretendard/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
     },
     {
-      path: "../fonts/Pretendard/Pretendard-Medium.woff2",
-      weight: "500",
-      style: "normal",
+      path: '../fonts/Pretendard/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'normal',
     },
     {
-      path: "../fonts/Pretendard/Pretendard-Bold.woff2",
-      weight: "700",
-      style: "normal",
+      path: '../fonts/Pretendard/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
     },
   ],
-  variable: "--pretendard",
-  display: "fallback",
+  variable: '--pretendard',
+  display: 'fallback',
 });
 const Gmarket = localFont({
   src: [
     {
-      path: "../fonts/Gmarket/GmarketSansLight.woff2",
-      weight: "100",
-      style: "normal",
+      path: '../fonts/Gmarket/GmarketSansLight.woff2',
+      weight: '100',
+      style: 'normal',
     },
     {
-      path: "../fonts/Gmarket/GmarketSansMedium.woff2",
-      weight: "500",
-      style: "normal",
+      path: '../fonts/Gmarket/GmarketSansMedium.woff2',
+      weight: '500',
+      style: 'normal',
     },
     {
-      path: "../fonts/Gmarket/GmarketSansBold.woff2",
-      weight: "900",
-      style: "normal",
+      path: '../fonts/Gmarket/GmarketSansBold.woff2',
+      weight: '900',
+      style: 'normal',
     },
   ],
-  variable: "--Gmarket",
-  display: "fallback",
+  variable: '--Gmarket',
+  display: 'fallback',
 });
 
 // 메타 데이터 설정
 export const metadata = {
-  title: "WIKO",
-  description: "WIKO",
+  title: 'WIKO',
+  description: 'WIKO',
   openGraph: {
-    type: "website",
-    title: "WIKO",
-    description: "WIKO",
-    images: "",
+    type: 'website',
+    title: 'WIKO',
+    description: 'WIKO',
+    images: '',
     // url: "https://WIKO.com",
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
-  keywords: "WIKO, wiko, ats, ATS, Wiko, Ats",
-  viewport: { width: "device-width", initialScale: 1.0 },
+  keywords: 'WIKO, wiko, ats, ATS, Wiko, Ats',
+  viewport: { width: 'device-width', initialScale: 1.0 },
 };
 
 export default function RootLayout({
@@ -84,6 +84,15 @@ export default function RootLayout({
     //   <body>{children}</body>
     // </html>
     <html lang="ko">
+      <head>
+        <script
+          type={'text/javascript'}
+          src={
+            '//dapi.kakao.com/v2/maps/sdk.js?appkey=77df11cd6f0bce2eee8159226e7ca2e5&libraries=services,clusterer&autoload=false'
+          }
+          defer
+        />
+      </head>
       <body className={`${pretendard.variable} ${Gmarket.variable}`}>
         <RecoilRootProvider>
           {/* header */}
@@ -91,7 +100,7 @@ export default function RootLayout({
 
           {/* content */}
           <ReactQueryProvider>
-            <div style={{ position: "relative", zIndex: "999" }}>
+            <div style={{ position: 'relative', zIndex: '999' }}>
               {children}
             </div>
             {/* <div className="wrap"></div> */}
